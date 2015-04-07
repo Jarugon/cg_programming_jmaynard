ba77f3d354dda7f9a804352a9a8b25ba9aafccb1
Jarugon <r202-stu@AI241LAB0202P06.aipdcls.edu>
2015-04-06 21:21:19 -0700

    first commit

---
+++ cg_programming_jmaynard/Application.h
@@ -0,0 +1,15 @@
+//Standard Headers...
+#pragma once
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cassert>
+#include <iostream>
+#include <cmath>
+
+//#include <GL/glew.h>
+
+//Name spaces...
+
+using namespace std;
---
+++ cg_programming_jmaynard/Main.cpp
@@ -0,0 +1,11 @@
+#include "Application.h"
+
+int main() {
+
+	cout <<"hello"<< endl;
+
+	//Game Loop...
+	do{
+		
+	}while(true);
+}
